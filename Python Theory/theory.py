6b4e5f71be9f1625399ebe813a7b47beeef32cc8
mrmbs1 <mbilalsadiq.19@gmail.com>
2025-08-28 15:31:20 +0100

    python 30 days

---
+++ Python Theory/theory.py
@@ -0,0 +1,16 @@
+"""Check version of python
+MacOS/Ubunutu = python3 --version
+Windows = python --version"""
+
+"""Run code in two different ways.
+python = Myscript.py or with python shell"""
+
+"""how to access python shell in terminal
+1. Type python3 in MacOS/Linux terminal
+2. You'll see >>> in prompt
+3. Test to see it works. e.g. type 2 + 2 and should return 4
+4. To exit prompt -> exit()"""
+
+
+#Single line comment we use hash
+""""We use triple quotes in python so we can write multiple lines of comments"""
---
+++ helloworld.py
@@ -0,0 +1,19 @@
+# print(2 + 3) #addition(+)
+# print(3 - 1) #subtraction(-)
+# print(2 * 3) #multiplication(*)
+# print(3 / 2) #divison(/)
+# print(3 ** 2) #exponential(**)
+# print(3 % 2) #modulus(%)
+# print(3 // 2) #Floor divison operator(//)
+
+#Checking data types
+
+print(type(10)) #Int
+print(type(3.14)) #float
+print(type(1 + 3j)) #complex number
+print(type('Bilaal')) #String
+print(type([1, 2, 3])) #List
+print(type({'name':'Bilaal'})) #Dictionary
+print(type({9.8, 3.14, 2.7})) #Set
+print(type((9.8, 3.14, 2.7))) #Tuple
+print(type(10)) #Tuple
