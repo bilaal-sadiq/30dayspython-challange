6ad6be13229cb8d23e348cf38236be2fd9e4fb64
mrmbs1 <mbilalsadiq.19@gmail.com>
2025-09-04 18:10:13 +0100

    Day1 complete

--- helloworld.py
+++ helloworld.py
@@ -8,12 +8,59 @@
 
 #Checking data types
 
-print(type(10)) #Int
-print(type(3.14)) #float
-print(type(1 + 3j)) #complex number
-print(type('Bilaal')) #String
-print(type([1, 2, 3])) #List
-print(type({'name':'Bilaal'})) #Dictionary
-print(type({9.8, 3.14, 2.7})) #Set
-print(type((9.8, 3.14, 2.7))) #Tuple
-print(type(10)) #Tuple
+# print(type(10)) #Int
+# print(type(3.14)) #float
+# print(type(1 + 3j)) #complex number
+# print(type('Bilaal')) #String
+# print(type([1, 2, 3])) #List
+# print(type({'name':'Bilaal'})) #Dictionary
+# print(type({9.8, 3.14, 2.7})) #Set
+# print(type((9.8, 3.14, 2.7))) #Tuple
+# print(type(10)) #Tuple
+
+
+#Exercise: Level 1 
+#Task 1
+""" 
+Open the python interactive shell and do the following operations. The operands are 3 and 4.
+addition(+)
+subtraction(-)
+multiplication(*)
+modulus(%)
+division(/)
+exponential(**)
+floor division operator(//)
+"""
+#Task 2
+
+"""Write Strings on the python interactive shell
+Your name
+Your family name
+Your country
+I am enjoying 30 days of python """
+
+# print(3+4)
+# print(4-3)
+# print(3*4)
+# print(4%3)
+# print(4/3)
+# print(4**3)
+# print(4//3)
+
+# print('Bilaal')
+# print('Sadiq')
+# print('UK')
+# print('I am enjoying 30 days of python')
+
+# print(type(10))
+# print(type(9.8))
+# print(type(4 - 4j))
+# print(type(['Bilaal', 'Afrah', 'Anaya']))
+# print(type('Bilaal'))
+
+p = [3]
+q = [2]
+
+print(math.dist(p, q))
+
+#Need to import math before running math functions
