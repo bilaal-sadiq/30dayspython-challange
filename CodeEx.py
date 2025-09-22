94be7138f4fb03ae81c063f4598170be707b264e
mrmbs1 <mbilalsadiq.19@gmail.com>
2025-09-22 18:13:04 +0100

    chapter 2 complete - variables

--- CodeEx.py
+++ CodeEx.py
@@ -1,13 +1,35 @@
-# username = input('Enter your name: ')
+# Here's a recap of everything we learned in this chapter:
+
+# Data types: int, float, str, bool.
+# Arithmetic operators: +, -, *, /.
+# The % modulo finds the remainder.
+# The ** exponentiation finds the exponent.
+# The input() function is used to get user input.
+# The int() function converts a value into an integer number.
+
+# username = input('What do you have left in name: ')
 # print(username)
 
 # age = int(input('What is your age?'))
-# print(age)
+# print(age
 
-num1 = int(input('Enter first number: '))
-num2 = int(input('Enter second number: '))
+# num1 = int(input('Enter first number: '))
+# num2 = int(input('Enter second number: '))
 
-total = (num1 ** 2) + (num2 ** 2)
+# total = (num1 ** 2) + (num2 ** 2)
 
 
-print(total)
+# print(total)
+
+# pesosEnter = int(input('What do you have left in pesos: '))
+# solesEnter = int(input('What do you have left in soles: '))
+# reaisEnter = int(input('What do you have left in reais: '))
+
+# pesos = 0.054 * pesosEnter
+# soles = 0.29 * solesEnter
+# reais = 0.19 * reaisEnter
+
+# totalDollars = pesos + soles + reais
+# print(totalDollars)
+
+""" CHAPTER 2 COMPLETE """
