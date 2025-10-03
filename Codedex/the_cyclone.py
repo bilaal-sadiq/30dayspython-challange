5e5a8025821d9d51a02e72c61b354562d8ad11ab
mrmbs1 <mbilalsadiq.19@gmail.com>
2025-10-03 22:25:11 +0100

    input data

---
+++ Codedex/the_cyclone.py
@@ -0,0 +1,18 @@
+# Logical operators, also known as Boolean operators, combine and evaluate two conditions. They are and, or, and not operators:
+
+# and returns True if both conditions are True, and returns False otherwise.
+# or returns True if at least one of the conditions is True, and False otherwise.
+# not returns True if the condition is False, and vice versa.
+
+# So you might be wondering: and and or are awfully similar, how do I remember the differences between the two? Let's break this down in a table form:
+
+# A	B	A and B	A or B
+# False	False	False	False
+# False	True	False	True
+# True	False	False	True
+# True	True	True	True
+
+height = int(input("How tall are you?  "))
+credits = int(input("How much credit d0 you have?  "))
+
+
