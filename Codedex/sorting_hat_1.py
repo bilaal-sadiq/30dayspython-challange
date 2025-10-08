4718304c580f677f78d18a63c10d0ce90b903ff9
mrmbs1 <mbilalsadiq.19@gmail.com>
2025-10-08 22:14:49 +0100

    control flow q2

---
+++ Codedex/sorting_hat_1.py
@@ -0,0 +1,70 @@
+# 🦁 Gryffindor
+# 🦅 Ravenclaw
+# 🦡 Hufflepuff
+# 🐍 Slytherin
+
+# import random
+
+# question = input("Do you like Dawn or Dusk")
+
+# Dawn = 1
+# Dusk = 2
+
+# random_answer = random.randint(1, 2)
+
+# if random_answer == 1:
+#     print("Gryffindor and Ravenclow get +1")
+# elif random_answer == 2:
+#     print("Hufflepuff and Slytherin get +1")
+# else :
+#     print("Wrong Output") 
+
+# gryffindor = 0
+# hufflepuff = 0
+# ravenclaw = 0
+# slytherin = 0
+
+# print('===============')
+# print('The Sorting Hat')
+# print('===============')
+
+# # ~~~~~~~~~~~~~~~ Question 1 ~~~~~~~~~~~~~~~
+
+# print('Q1) Do you like Dawn or Dusk?')
+
+# print('  1) Dawn')
+# print('  2) Dusk')
+
+# answer = int(input('Enter answer (1-2): '))
+
+# if answer == 1:
+#   gryffindor = gryffindor + 1
+#   ravenclaw = ravenclaw + 1
+# elif answer == 2:
+#   hufflepuff = hufflepuff + 1
+#   slytherin = slytherin + 1
+# else:
+#   print('Wrong input.')
+
+# ~~~~~~~~~~~~~~~ Question 1 ~~~~~~~~~~~~~~~
+
+
+print("When I'm gone, remember me as: ")
+
+print(" 1) The Good")
+print(" 2) The Great")
+print(" 3) The Wise")
+print(" 4) The Bold")
+
+answer = int(input('Enter your answer (1-4): '))
+
+if answer == 1:
+    print("Hufflepuff +2")
+elif answer == 2:
+    print("Slytherin +2")
+elif answer == 3:
+    print("Ravencalw +2")
+elif answer == 4:
+    print("Gryffindor +2")
+else:
+    print("Wrong output")
